05e3e6b96eccf40a31c39560979a1583c3a7a415
joonyoo181 <joon.yoo181@berkeley.edu>
2023-10-29 01:32:12 -0700

    Text to speech works

--- frontend/App.js
+++ frontend/App.js
@@ -7,12 +7,14 @@ import {
   Button
 } from 'react-native';
 import { Camera } from 'react-native-vision-camera';
+import Tts from 'react-native-tts';
 
 function App() {
   const camera = useRef(null);
   const [isCameraReady, setIsCameraReady] = useState(false);
   const devices = Camera.getAvailableCameraDevices();
   const device = devices.find((d) => d.position === 'back');
+  const lastSpeech = useRef(null);
 
   const takePhoto = async () => {
     const photo = await camera.current.takePhoto({
@@ -23,12 +25,33 @@ function App() {
     console.log(photo);
   }
 
+  const speak = (text) => {
+    if (lastSpeech.current !== null) {
+      // If there's an ongoing speech, stop it before starting a new one
+      stopSpeaking();
+    }
+    lastSpeech.current = text;
+    Tts.speak(text, { forceStop: true });
+  };
+
+  const stopSpeaking = () => {
+    Tts.stop();
+    lastSpeech.current = null;
+  };
+
   useEffect(() => {
     async function getPermission() {
       const newCameraPermission = await Camera.requestCameraPermission();
       console.log(newCameraPermission);
     }
     getPermission();
+
+    // TTS config
+    Tts.setDefaultLanguage('en-US');
+
+    // Optionally, adjust the speed and pitch
+    Tts.setDefaultRate(0.8);
+    Tts.setDefaultPitch(1);
   }, []);
 
   setInterval(() => {
